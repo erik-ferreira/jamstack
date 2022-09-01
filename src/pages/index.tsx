3e46e5c1f1b2e3d1d46b387aea6f1ebbf5ea711f
erik-ferreira <erikdesouzagf@gmail.com>
2022-09-01 00:31:12 -0300

    added configs codegen; part of the class completed

---
+++ src/pages/index.tsx
@@ -0,0 +1,7 @@
+import { usePageQuery } from "../generated/graphql";
+
+export default function Home() {
+  const [{ data }] = usePageQuery({ variables: { slug: "home" } });
+
+  return <h1>{data?.page.title}</h1>;
+}
