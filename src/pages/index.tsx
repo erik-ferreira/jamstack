69989fe73015fc05b9f61cff7eb86f67785bdad7
erik-ferreira <erikdesouzagf@gmail.com>
2022-09-01 23:56:32 -0300

    adde new queries and config to use cache

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,20 @@
-import { usePageQuery } from "../generated/graphql";
+import { GetServerSideProps } from "next";
+
+import { client, ssrCache } from "../lib/urql";
+import { PageDocument, usePageQuery } from "../generated/graphql";
 
 export default function Home() {
   const [{ data }] = usePageQuery({ variables: { slug: "home" } });
 
   return <h1>{data?.page.title}</h1>;
 }
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  await client.query(PageDocument, { slug: "home" }).toPromise();
+
+  return {
+    props: {
+      urqlState: ssrCache.extractData(),
+    },
+  };
+};
